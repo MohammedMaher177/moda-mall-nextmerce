4da47f193e190724e0e2b34aef4ee8b5ee3a1386
anahammo777@gmail.com <anahammo777@gmail.com>
2025-10-27 14:15:51 +0300

    .

--- src/app/(site)/(pages)/my-account/page.tsx
+++ src/app/(site)/(pages)/my-account/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "My Account | NextCommerce Nextjs E-commerce template",
-  description: "This is My Account page for NextCommerce Template",
+  title: "My Account | E-commerce Store",
+  description: "This is My Account Page for Moda Mall Store",
   // other metadata
 };
 
